6760a2a7899ce0a2003697772df5a3cad9a21338
Fox_white <vct.xie@gmail.com>
2021-12-05 22:29:26 +0800

    Simplify IDENTIFY event

--- qq/gateway.py
+++ qq/gateway.py
@@ -235,14 +235,6 @@ class QQdWebSocket:
             }
         }
 
-        if self.shard_id is not None and self.shard_count is not None:
-            payload['d']['shard'] = [self.shard_id, self.shard_count]
-
-        state = self._connection
-
-        if state._intents is not None:
-            payload['d']['intents'] = state._intents.value
-
         await self.call_hooks('before_identify', self.shard_id, initial=self._initial_identify)
         await self.send_as_json(payload)
         _log.info('Shard ID %s has sent the IDENTIFY payload.', self.shard_id)
