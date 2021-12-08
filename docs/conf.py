85f94bd3fc2c353773313d40f7133f369f73d75e
Fox_white <vct.xie@gmail.com>
2021-12-09 00:54:00 +0800

    Asset doc

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'qq.py'
-copyright = '2015-present, Rapptz'
+copyright = '2021-现在, foxwhite25'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -292,7 +292,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     ('index', 'qq.py.tex', 'qq.py Documentation',
-     'Rapptz', 'manual'),
+     'foxwhite25', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -322,7 +322,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'qq.py', 'qq.py Documentation',
-     ['Rapptz'], 1)
+     ['foxwhite25'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -336,7 +336,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
     ('index', 'qq.py', 'qq.py Documentation',
-     'Rapptz', 'qq.py', 'One line description of project.',
+     'foxwhite25', 'qq.py', 'One line description of project.',
      'Miscellaneous'),
 ]
 
