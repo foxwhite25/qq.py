a164f387255416f9ed07de5a28fc206d1ab655f2
Fox_white <vct.xie@gmail.com>
2021-12-24 19:52:46 +0800

    :bulb: [Documenting code] Fix example

--- examples/new_member.py
+++ examples/new_member.py
@@ -7,7 +7,7 @@ class MyClient(qq.Client):
         print('------')
 
     async def on_member_join(self, member: qq.Member):
-        channel = member.guild.fetch_channel(114514)  # 频道ID
+        channel = await member.guild.fetch_channel(114514)  # 频道ID
         if channel is not None:
             to_send = f'欢迎 {member.mention} 加入 {member.guild.name}！'
             await channel.send(to_send)
