6d8d8c8c9d0db973a84167d7ac8a1f5ba2e8f8b8
Fox_white <vct.xie@gmail.com>
2021-12-24 20:05:07 +0800

    :bookmark: [Version tag] v1.0.9

--- qq/__init__.py
+++ qq/__init__.py
@@ -2,7 +2,7 @@ __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 __title__ = 'qq'
 __author__ = 'Foxwhite'
 __license__ = 'MIT'
-__version__ = '1.0.8'
+__version__ = '1.0.9'
 
 import logging
 from typing import NamedTuple, Literal
@@ -36,4 +36,4 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=1, minor=0, micro=8, releaselevel='beta', serial=0)
+version_info: VersionInfo = VersionInfo(major=1, minor=0, micro=9, releaselevel='beta', serial=0)
