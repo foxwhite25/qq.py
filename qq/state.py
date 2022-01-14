a7075dca468b50e0295f07a1416be6fb26aee596
Fox_white <vct.xie@gmail.com>
2022-01-14 18:10:14 +0800

    :sparkles: [Feature] Separate At and not at

--- qq/state.py
+++ qq/state.py
@@ -412,7 +412,15 @@ class ConnectionState:
             channel.last_message_id = message.id  # type: ignore
 
     def parse_message_create(self, data) -> None:
-        self.parse_at_message_create(data)
+        channel, _ = self._get_guild_channel(data)
+        # channel would be the correct type here
+        message = Message(channel=channel, data=data, state=self)  # type: ignore
+        self.dispatch('message', message)
+        if self._messages is not None:
+            self._messages.append(message)
+        # we ensure that the channel is either a TextChannel or Thread
+        if channel and channel.__class__ in (TextChannel,):
+            channel.last_message_id = message.id  # type: ignore
 
     def parse_channel_delete(self, data) -> None:
         guild = self._get_guild(data.get('guild_id'))
