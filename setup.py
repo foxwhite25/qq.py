26c897a13c515d018190d1ee73a3aea1879d7f2a
Fox_white <vct.xie@gmail.com>
2022-01-19 05:04:42 +0800

    :bookmark: [Version tag] 1.1.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ extras_require = {
 
 setup(
     name='qq.py',
-    version='1.1.1',
+    version='1.1.2',
     description='QQ 频道 API 的 Python Wrapper',
     py_modules=["qq"],
     packages=['qq', "qq.types", "qq.ext.commands", "qq.ext.tasks"],
