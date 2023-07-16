758a67955d4b317ac4cd8aabfdc4002f0d87820f
Fox_white <vct.xie@gmail.com>
2023-07-16 22:03:55 +0800

    :bookmark: [Version tag] v1.2.16

--- qq/__init__.py
+++ qq/__init__.py
@@ -2,7 +2,7 @@ __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 __title__ = 'qq'
 __author__ = 'Foxwhite'
 __license__ = 'MIT'
-__version__ = '1.2.15'
+__version__ = '1.2.16'
 
 #  The MIT License (MIT)
 #  Copyright (c) 2021-present foxwhite25
@@ -61,4 +61,4 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=1, minor=2, micro=15, releaselevel='beta', serial=0)
+version_info: VersionInfo = VersionInfo(major=1, minor=2, micro=16, releaselevel='beta', serial=0)
